a8b2defd7fe70704926e32b40ed4e11968fdbd2c
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-06-02 17:05:39 +0300

    Update singlepost.component.ts

--- src/app/singlepost/singlepost.component.ts
+++ src/app/singlepost/singlepost.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Post } from '../addpostform/post';
 import { User } from '../registerform/user';
