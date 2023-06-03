673d3f25a366cc05319eca571466088822e13e61
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-06-03 18:30:19 +0300

    Update post.service.ts

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,61 +1,89 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, tap, throwError, Subject } from 'rxjs';
+import { catchError, Observable,tap, throwError} from 'rxjs';
 import { Post } from '../addpostform/post';
-
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-  postAdded$: Subject<any> = new Subject<any>();
 
-  constructor(private httpReq: HttpClient) { }
-  
-  // Diğer metodlar...
+  constructor(private httpReq:HttpClient) { }
+  path="https://techlog-backend.onrender.com/api/posts";
 
-  createPost(post: Post): Observable<any> {
+  getall():Observable<any>{
     try {
-      let newpath = this.path + "/new";
-      return this.httpReq.post(newpath, post).pipe(
+      let newpath =this.path+"/All";
+      return this.httpReq.get(newpath).pipe(tap(data=>{
+        console.log(data);
+        return data;
+      }));
+    } catch (err) {
+      console.log("message of err:",err.message);
+      let error= err;
+      return error;
+    }
+  };
+  getPostsByUserId(Id:string): Observable<any> {
+    try {
+      let newPath = this.path + "/All/" + Id; // add userId to the URL
+      return this.httpReq.get(newPath).pipe(
         tap(data => {
-          console.log(data._id);
-          this.postAdded$.next(data); // Yeni bir gönderi oluşturulduğunda postAdded$ özelliğine sinyal gönderiyoruz
+          console.log("userid:",data.userid,"data:",data);
           return data;
         })
       );
     } catch (err) {
-      console.log("message of err:", err);
-      return throwError(err);
+      console.log("Error message:", err);
+      let error = err;
+      return error;
     }
   }
-
-  deletePost(id: string): Observable<any> {
-    let newPath = this.path + "/delete/" + id;
-    return this.httpReq.delete(newPath).pipe(
-      tap(data => {
-        console.log("delete res:", data);
-        this.postAdded$.next(data); // Bir gönderi silindiğinde postAdded$ özelliğine sinyal gönderiyoruz
-      })
-    );
-  }
-
-  addLike(id: string, userId: string): Observable<any> {
-    let newPath = `${this.path}/like/${id}/${userId}`;
+  getOne(id: string): Observable<any> {
+    let newPath = this.path + "/one/" + id;
     return this.httpReq.get(newPath).pipe(
       tap(data => {
-        console.log("data of postLike:", data);
-        this.postAdded$.next(data); // Bir gönderiye beğeni eklendiğinde postAdded$ özelliğine sinyal gönderiyoruz
+        console.log("onepost:", data);
+      }),
+      catchError(err => {
+        console.log("hata:", err);
+        return throwError(err);
       })
     );
   }
 
-  disLike(id: string, userId: string): Observable<any> {
-    let newPath = `${this.path}/dislike/${id}/${userId}`;
-    return this.httpReq.get(newPath).pipe(
-      tap(data => {
-        console.log("data of postDisLike:", data);
-        this.postAdded$.next(data); // Bir gönderiden beğeni kaldırıldığında postAdded$ özelliğine sinyal gönderiyoruz
-      })
-    );
+
+
+
+  createPost(post:Post):Observable<any>{
+    try {
+      let newpath =this.path+"/new";
+      return this.httpReq.post(newpath,post).pipe(tap(data=>{
+        console.log(data._id);
+        return data;
+      }));
+    } catch (err) {
+      console.log("message of err:",err);
+      let error= err;
+      return error;
+    }
+  };
+  deletepost(id:string):Observable<any>{
+    let newPath = this.path+"/delete/"+id;
+    return this.httpReq.delete(newPath).pipe(tap(data=>{
+      console.log("delet res:",data);
+    }))
   }
+  addLike(id:string,userid:string): Observable<any>{
+    let newPath = `${this.path}/like/${id}/${userid}`;
+    return this.httpReq.get(newPath).pipe(tap(data=>{
+      console.log("data of postLike:",data);
+    }));
+  }
+  disLike(id:string,userid:string): Observable<any>{
+    let newPath = `${this.path}/dislike/${id}/${userid}`;
+    return this.httpReq.get(newPath).pipe(tap(data=>{
+      console.log("data of postDisLike:",data);
+    }));
+  }
+
 }
