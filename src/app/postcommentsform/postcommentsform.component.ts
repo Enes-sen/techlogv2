a6a4eda0a0ad5bf506a739d008167549b58a5f8e
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-06-06 15:01:51 +0300

    Update postcommentsform.component.ts

--- src/app/postcommentsform/postcommentsform.component.ts
+++ src/app/postcommentsform/postcommentsform.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit,OnChanges, SimpleChanges } from '@angular/core';
+// PostcommentsformComponent
+
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Post } from '../addpostform/post';
 import { AlertifyService } from '../services/alertify.service';
 import { AuthService } from '../services/auth.service';
 import { CommentService } from '../services/comment.service';
 import { PostService } from '../services/post.service';
 import { CommEnt } from './comment';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-postcommentsform',
   templateUrl: './postcommentsform.component.html',
   styleUrls: ['./postcommentsform.component.css'],
 })
-export class PostcommentsformComponent implements OnInit {
+export class PostcommentsformComponent implements OnInit{
   comentedpost: Post;
   commentModel: CommEnt = new CommEnt();
   Id = localStorage.getItem('loggedUser');
@@ -23,49 +24,31 @@ export class PostcommentsformComponent implements OnInit {
   serverurl = 'https://techlog-backend.onrender.com/api/users';
   id: any;
   commentsofpost: CommEnt[];
-  commentAdded$: Subject<void>;
 
   constructor(
     private postServ: PostService,
     private alertServ: AlertifyService,
     private authServ: AuthService,
-    private routers: ActivatedRoute,
-    private commentServ: CommentService,
-    private routes: Router
-  ) {
-    this.commentAdded$ = new Subject<void>();
-  }
+    private router: ActivatedRoute,
+    private commentServ: CommentService
+  ) {}
 
   ngOnInit(): void {
-    this.routers.params.subscribe((params) => {
+    this.router.params.subscribe((params) => {
       const _id = params['id'];
       this.id = _id;
       this.destination += params['id'];
       this.postServ.getOne(_id).subscribe((data) => {
         try {
           this.comentedpost = data.post;
-          console.log('comentpost:', this.comentedpost);
-          this.getComments(); // İlk başta yorumları getir
+          console.log('comment post:', this.comentedpost);
+          this.commentServ.onChanges().subscribe(() => {
+            this.getComments(); // Fetch updated comments
+          });
         } catch (err) {
-          console.log('hata:', err.message);
+          console.log('error:', err.message);
           this.alertServ.danger(err.message);
         }
-      });
-    });
-  }
-   ngOnChanges(changes: SimpleChanges) {
-    if (changes['commentsofpost']) {
-      console.log('commentsofpost değişti:', this.commentsofpost);
-    }
-  }
-
-  getComments(): void {
-    this.commentServ.getAll(this.id).subscribe((data) => {
-      this.commentsofpost = data.comments;
-      this.commentsofpost.sort((a, b) => {
-        const dateA = new Date(a.createdAt);
-        const dateB = new Date(b.createdAt);
-        return dateB.getTime() - dateA.getTime();
       });
     });
   }
@@ -76,25 +59,34 @@ export class PostcommentsformComponent implements OnInit {
 
   addComment(form: NgForm) {
     if (form.invalid) {
-      return this.alertServ.danger('Yorum ekleme başarısız!');
+      return this.alertServ.danger('Failed to add comment!');
     }
-    
     this.commentModel.user = this.user;
     this.commentModel.post = this.comentedpost;
-
-    if (this.commentModel.user !== null && this.commentModel.post !== null) {
-      this.commentServ.AddnewComment(this.commentModel).subscribe((data) => {
-        console.log('data of comment:', data);
+    if (this.commentModel.user && this.commentModel.post) {
+      this.commentServ.addNewComment(this.commentModel).subscribe((data) => {
+        console.log('comment data:', data);
         if (data.success === false) {
-          this.alertServ.danger('Hata:' + data.message);
+          this.alertServ.danger('Error: ' + data.message);
         } else {
-          this.alertServ.success('Yorum eklendi');
-          this.getComments(); // Yeni yorum eklendiğinde güncellemeyi tetikle
-          form.resetForm(); // Yorum formunu sıfırla
+          this.alertServ.success('Comment added');
+          this.commentServ.commentAddedSuccessfully();
+          form.resetForm(); // Reset the comment form
         }
       });
     } else {
-      this.alertServ.danger('Beklenmedik bir hata oluştu!');
+      this.alertServ.danger('An unexpected error occurred!');
     }
+  }
+
+  getComments(): void {
+    this.commentServ.getAll(this.id).subscribe(data => {
+      this.commentsofpost = data.comments;
+      this.commentsofpost.sort((a, b) => {
+        const dateA = new Date(a.createdAt);
+        const dateB = new Date(b.createdAt);
+        return dateB.getTime() - dateA.getTime();
+      });
+    });
   }
 }
